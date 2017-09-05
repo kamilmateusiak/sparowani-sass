3a2743032fe5345a55f5d3e64ec59659dc4bd63e
unknown <grzanka.katarzynaa@gmail.com>
2017-09-05 20:04:14 +0200

    skrolowanie

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('ready1');
 });
+
+            
+                
+$.scrollTo(0);
+$('.nav a').click(function() { $.scrollTo($(this).attr('href'), 500); });
+  
+<script src="dist/js/main.js"></script>
+
+$(document).ready(function() {
+	var NavY = $('.rectangle').offset().top;
+	 
+	var stickyNav = function(){
+	var ScrollY = $(window).scrollTop();
+		  
+	if (ScrollY > NavY) { 
+		$('.rectangle').addClass('sticky');
+	} else {
+		$('.rectangle').removeClass('sticky'); 
+	}
+	};
+	 
+	stickyNav();
+	 
+	$(window).scroll(function() {
+		stickyNav();
+	});
+	});
