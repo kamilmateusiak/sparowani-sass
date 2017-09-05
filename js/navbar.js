3fb74dd2ae8a1635a2f0c769725078c55af28145
unknown <grzanka.katarzynaa@gmail.com>
2017-09-05 21:19:39 +0200

    poprawki

--- js/navbar.js
+++ js/navbar.js
@@ -1,17 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('ready1');
-});
-
-            
-                
-$.scrollTo(0);
-$('.nav a').click(function() { $.scrollTo($(this).attr('href'), 500); });
-  
-<script src="dist/js/main.js"></script>
-
 $(document).ready(function() {
 	var NavY = $('.rectangle').offset().top;
-	 
+    
 	var stickyNav = function(){
 	var ScrollY = $(window).scrollTop();
 		  
@@ -22,9 +11,9 @@ $(document).ready(function() {
 	}
 	};
 	 
-	stickyNav();
+	   stickyNav();
 	 
-	$(window).scroll(function() {
-		stickyNav();
+	   $(window).scroll(function() {
+ 		stickyNav();
 	});
 	});
