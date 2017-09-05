53d70c389ba0ff697059a8fe13d8ea1c1cdd3525
vagabondi <kamil.mateusiak@gmail.com>
2017-09-05 21:46:51 +0200

    small fixes

--- js/navbar.js
+++ js/navbar.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
+	$.scrollTo(0);
+	$('.nav a').click(function() { $.scrollTo($(this).attr('href'), 500); });
+
 	var NavY = $('.rectangle').offset().top;
     
 	var stickyNav = function(){
-	var ScrollY = $(window).scrollTop();
-		  
-	if (ScrollY > NavY) { 
-		$('.rectangle').addClass('sticky');
-	} else {
-		$('.rectangle').removeClass('sticky'); 
-	}
+		var ScrollY = $(window).scrollTop();
+			
+		if (ScrollY > NavY) { 
+			$('.rectangle').addClass('sticky');
+		} else {
+			$('.rectangle').removeClass('sticky'); 
+		}
 	};
 	 
-	   stickyNav();
+	stickyNav();
 	 
-	   $(window).scroll(function() {
+	$(window).scroll(function() {
  		stickyNav();
 	});
-	});
+});
